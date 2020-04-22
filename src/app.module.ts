5d0930db2f1445f1ec14f0c1202bdbfe6245ffec
Dmitrii Kupchenko <kupchenko.work@gmail.com>
2020-04-23 00:29:18 +0300

    Config app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import {Module} from '@nestjs/common';
-import {UserModule} from './entity/user/user.module';
+import { Module } from '@nestjs/common';
+import { UserModule } from './entity/user/user.module';
+import { AuthModule } from './entity/auth/auth.module';
+import { databaseProviders } from './database/database.providers';
 
 @Module({
-    imports: [UserModule],
-    controllers: [],
-    providers: [],
+  imports: [UserModule, AuthModule, ...databaseProviders],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {
 }
